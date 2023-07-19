c52cd87d5783e44c56dd6ffd2cfe9b59ce44c361
syedzubairahmad8446 <127229486+syedzubairahmad8446@users.noreply.github.com>
2023-07-19 21:24:00 +0530

    Add files via upload

---
+++ syed zubair python lec 01.py
@@ -0,0 +1,349 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import this
+
+
+# In[2]:
+
+
+print("this is my first prog")
+
+
+# In[37]:
+
+
+a=20
+
+
+# In[38]:
+
+
+a
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+zubb =8446
+
+
+# In[ ]:
+
+
+zubbu=9657
+
+
+# In[7]:
+
+
+type(a)
+
+
+# In[10]:
+
+
+b=12.25
+
+
+# In[11]:
+
+
+type(b)
+
+
+# In[12]:
+
+
+c= "zubb"
+
+
+# In[13]:
+
+
+type(c)
+
+
+# In[14]:
+
+
+d=5+6j
+   
+
+
+# In[15]:
+
+
+type(d)
+
+
+# In[16]:
+
+
+d
+
+
+# In[17]:
+
+
+e="zubb"
+
+
+# In[18]:
+
+
+e
+
+
+# In[20]:
+
+
+n="dnnnngghhgnnnnnngnnnhhhhyyyyyyyy"
+
+
+# In[21]:
+
+
+n
+
+
+# In[25]:
+
+
+g=True  
+
+
+# In[26]:
+
+
+type(g)
+
+
+# In[27]:
+
+
+*a=67
+
+
+# In[28]:
+
+
+_a=34
+
+
+# In[43]:
+
+
+a = 21
+
+
+# In[44]:
+
+
+a
+
+
+# In[51]:
+
+
+a=5464
+b="zubb"
+c=6+7j
+d=True
+e=546.67
+
+
+# In[52]:
+
+
+a
+
+
+# In[64]:
+
+
+a1=100
+
+
+# In[68]:
+
+
+a1+a
+
+
+# In[89]:
+
+
+b
+
+
+# In[90]:
+
+
+c
+
+
+# In[91]:
+
+
+d
+
+
+# In[92]:
+
+
+e
+
+
+# In[103]:
+
+
+a,b,c,d,e= 100, "syed", 5+6j, 350.54,False,
+
+
+# In[95]:
+
+
+a
+
+
+# In[96]:
+
+
+b
+
+
+# In[104]:
+
+
+c
+
+
+# In[105]:
+
+
+type(c)
+
+
+# In[106]:
+
+
+c.real
+
+
+# In[107]:
+
+
+c.imag
+
+
+# In[108]:
+
+
+b
+
+
+# In[114]:
+
+
+a = "zubb"
+
+
+# In[115]:
+
+
+a+4
+
+
+# In[116]:
+
+
+a +str(4)
+
+
+# In[117]:
+
+
+True + True
+
+
+# In[1]:
+
+
+True - False
+
+
+# In[2]:
+
+
+True+1
+
+
+# In[119]:
+
+
+1 + True
+
+
+# In[1]:
+
+
+input()
+
+
+# In[ ]:
+
+
+a = "zubb"
+
+
+# In[2]:
+
+
+a = input()
+
+
+# In[3]:
+
+
+a
+
+
+# In[5]:
+
+
+b = 25
+
+
+# In[6]:
+
+
+int(a)+b
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
